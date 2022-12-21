87dbc9697d7febd04b6e590a8b6cd1d6f4e880da
Shivank <shivank.shuman@gmail.com>
2022-12-21 20:51:34 +1100

    added python to tree-sitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = { "lua", "javascript", "rust", "clojure", "fish", "query", "java" },
+  ensure_installed = { "lua", "javascript", "rust", "clojure", "fish", "query", "java", "python" },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
