6f39cc0b5a4d60b91e30971385e83acb7740dac3
Shivank <shivank.shuman@gmail.com>
2023-01-10 17:42:06 +1100

    Tokyonight setup

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,32 @@
 -- configure tokyonight
-vim.g.tokyonight_style = "storm"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+local settings = {
+  style = "moon",
+  light_style = "day",
+  transparent = false,
+  terminal_colors = true,
+  styles = {
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    functions = {},
+    variables = {},
+    keywords = { italic = true }
+  },
+  sidebars = { "qf", "help" },
+  day_brightness = 0.3,
+  on_colors = function (colors)
+    colors.hint = colors.blue2
+    colors.error = "#ff0000"
+  end,
+}
+
+
+local status_ok, tokyonight = pcall(require, "tokyonight")
+if not status_ok then
+  return
+end
+
+
+tokyonight.setup(settings)
 
 -- enable tokyonight
 vim.cmd [[
