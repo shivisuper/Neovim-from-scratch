b3ec538d484ed3238b40cd8c21cbc90986c4a46f
Shivank <shivank.shuman@gmail.com>
2023-01-14 15:47:48 +1100

    enable rust tools for rust lsp

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -48,26 +48,26 @@ for _, server in pairs(servers) do
     opts = vim.tbl_deep_extend("force", conf_opts, opts)
   end
 
-  --if server == "rust_analyzer" then
+  if server == "rust_analyzer" then
 
-  --require("rust-tools").setup {
-  --tools = {
-  -- on_initialized = function()
-  --  vim.cmd [[
-  --   autocmd BufEnter,CursorHold,InsertLeave,BufWritePost *.rs silent! lua vim.lsp.codelens.refresh()
-  --]]
-  --end,
-  --},
-  --server = {
-  -- on_attach = require("user.lsp.handlers").on_attach,
-  -- capabilities = require("user.lsp.handlers").capabilities,
-  -- settings = require("user.lsp.settings.rust_analyzer").settings,
-  --},
-  -- }
+    require("rust-tools").setup {
+      tools = {
+        on_initialized = function()
+          vim.cmd [[
+     autocmd BufEnter,CursorHold,InsertLeave,BufWritePost *.rs silent! lua vim.lsp.codelens.refresh()
+     ]]
+        end,
+      },
+      server = {
+        on_attach = require("user.lsp.handlers").on_attach,
+        capabilities = require("user.lsp.handlers").capabilities,
+        settings = require("user.lsp.settings.rust_analyzer").settings,
+      },
+    }
 
-  --goto continue
-  --end
+    goto continue
+  end
 
   lspconfig[server].setup(opts)
-  --::continue::
+  ::continue::
 end
